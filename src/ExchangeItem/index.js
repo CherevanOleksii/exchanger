75ec6c9f6c7987c7ffc10f5a3f22b93b6974a8b8
alex <lolmagexe@gmail.com>
2021-11-17 06:56:46 +0200

    finish comment ExchangeForm

--- src/ExchangeItem/index.js
+++ src/ExchangeItem/index.js
@@ -1,28 +1,28 @@
 import { useEffect, useState } from 'react';
 import './style.scss'
 
-const ExchangeItem = ({ img, title = '', amount = '', inputValue = '', mainCurrency = '', currency = '', callbackInput = null }) => {
+const ExchangeItem = ({ img, title = '', amount = '', inputValue = '', mainCurrency = '', currency = '', callbackInput = null }) => { // Передаем аргументы в параметры, деконструктор чтобы не использовать входящий параметр "props"
 
-    const [value, setValue] = useState(inputValue)
+    const [localInputValue, setLocalInputValue] = useState(inputValue) // Хук состояние, храним значение ввода
 
-    useEffect(() => {
-        setValue(inputValue)
-    }, [inputValue])
+    // useEffect(() => { 
+    //     setLocalInputValue(inputValue)
+    // }, [inputValue])
 
 
     const handleInput = (event) => {
         const val = event.target.value;
-        // Если нужно задать ограничени 
+        // Если нужно задать ограничение на количество цивр после запятой 
         // const re =/^[0-9]+(?:[\.]|[\.][0-9]{1,4})?$/
-        const re =/^[0-9]+(?:[\.]|[\.][0-9]+)?$/
+        const re =/^[0-9]+(?:[\.]|[\.][0-9]+)?$/ // Регулярное выражение для поиска числа, наш string должен быть СТРОГО ЧИСЛОМ!
 
-        if (val === '' || re.test(val)) {
-            setValue(val);
-            callbackInput(val);
+        if (val === '' || re.test(val)) { // Проверяем подходит ли нам стоки, если да, то присваеваем новое состояние строки
+            setLocalInputValue(val); // Присваеваем значение компонента
+            callbackInput(val); // Передаем значение родителю
         }
     }
 
-    return (
+    return ( // Возвращаем компонент
 
         <div className='exchange-item'>
             <div className={'exchange-item-left'}>
@@ -34,7 +34,7 @@ const ExchangeItem = ({ img, title = '', amount = '', inputValue = '', mainCurre
                 <div className='exchange-item-description'>Rate: {amount} {mainCurrency}</div>
 
                 <div className='exchange-item-bot-container'>
-                    <input className='exchange-item-input' value={value} onChange={handleInput}></input>
+                    <input className='exchange-item-input' value={localInputValue} onChange={handleInput}></input>
                 </div>
             </div>
             <div className="exchange-item-selected-currency">
--- src/ExchangeOperation/index.js
+++ src/ExchangeOperation/index.js
@@ -1,29 +1,39 @@
 import { useEffect, useReducer, useState } from 'react'
 import './style.scss'
 
-const ExchagneOperation = ({ callbackIsBuy }) => {
+const ExchagneOperation = ({ callbackIsBuy }) => { // Передаем аргументы в параметры, деконструктор чтобы не использовать входящий параметр "props"
 
-    const btnOperationStyle = {
-        active: {
+    const btnOperationStyle = { // Шаблон "стиль состояние кнопки"
+        active: { // Стиль активной кнопки
             backgroundColor: '#daeef9',
             color: '#222222',
             fontWeight: "900",
             cursor: 'default'
         },
-        disable: {
+        disable: { // Стиль не активной кнопки
             backgroundColor: '#edf7fd',
             backgound: 'CADETBLUE',
             color: '#1819b3',
             textDecorationLine: 'underline',
         }
     }
+    
+    const operationInitState = { // Шаблон инициализации 
+        isBuy: true,
+        btnStyleBuy: {
+            ...btnOperationStyle.active
+        },
+        btnStyleSell: {
+            ...btnOperationStyle.disable
+        }
+    }
 
 
-    const operationReducer = (state, action) => {
+    const operationReducer = (state, action) => { // Метод редусера операций
 
-        switch (action.type) {
-            case "BUY":
-                return {
+        switch (action.type) { // Распознаем нужный нам тип
+            case "BUY": // Нажатие на кнопку купить
+                return { // ВОзвражаем новое состояне
                     isBuy: true,
                     btnStyleBuy: {
                         ...btnOperationStyle.active
@@ -32,8 +42,8 @@ const ExchagneOperation = ({ callbackIsBuy }) => {
                         ...btnOperationStyle.disable
                     }
                 }
-            case "SELL":
-                return {
+            case "SELL": // При нажатии кнопки продать 
+                return { // Возвращаем новое состояние
                     isBuy: false,
                     btnStyleBuy: {
                         ...btnOperationStyle.disable
@@ -50,39 +60,33 @@ const ExchagneOperation = ({ callbackIsBuy }) => {
     }
 
 
-    const [operation, dispacherOperation] = useReducer(operationReducer,
-        {
-            isBuy: true,
-            btnStyleBuy: {
-                ...btnOperationStyle.active
-            },
-            btnStyleSell: {
-                ...btnOperationStyle.disable
-            }
-        })
+    const [operation, dispacherOperation] = useReducer( // Хук reducer
+        operationReducer, // Метод reducer
+        operationInitState // Начальное состояние
+        )
 
 
 
-    useEffect(() => {
+    useEffect(() => { // При переключении "isBuy" вызываем callback родителя, передаем состояние isBuy
         callbackIsBuy(operation.isBuy)
     }, [operation])
 
-
-    const handleButtonSell = (event) => {
+ 
+    const handleButtonSell = (event) => { // Событие нажатия кнопки "продать"
         dispacherOperation({
             type: "SELL"
         })
     }
 
 
-    const handleButtonBuy = (event) => {
+    const handleButtonBuy = (event) => { // Событие нажатия кнопки "купить"
         dispacherOperation({
             type: "BUY"
         })
     }
 
 
-    return (
+    return ( // Возвращаем компонент
         <>
             <div className='exchange-operation'>
                 <button className='button-buy' style={{ ...operation.btnStyleBuy }} onClick={() => handleButtonBuy()}> Buy </button>
